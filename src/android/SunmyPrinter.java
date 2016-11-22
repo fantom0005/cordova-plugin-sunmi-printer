72b96fe4d06004c2a37248f67a65e69afdd10615
Anton Shvetsov <antonshvecow@gmail.com>
2016-11-22 16:53:32 +0700

    Test

--- src/android/SunmyPrinter.java
+++ src/android/SunmyPrinter.java
@@ -8,6 +8,12 @@ import  ru.fantom.sunmi.sunmyprinter.IWoyouService;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
+import android.os.RemoteException;
 
 /**
  * This class echoes a string called from JavaScript.
@@ -26,9 +32,53 @@ public class SunmyPrinter extends CordovaPlugin {
 
     private void coolMethod(String message, CallbackContext callbackContext) {
         if (message != null && message.length() > 0) {
+            this.initPrinter();
+            this.print(message)
             callbackContext.success(message);
         } else {
             callbackContext.error("Expected one non-empty string argument.");
         }
     }
+
+    private static final SunmyPrinter printer = new SunmyPrinter();
+	private  IWoyouService woyouService = null;
+	
+	private ServiceConnection connService = new ServiceConnection() {
+
+		@Override
+		public void onServiceDisconnected(ComponentName name) {
+			woyouService = null;
+		}		
+		@Override
+		public void onServiceConnected(ComponentName name, IBinder service) {
+			woyouService = IWoyouService.Stub.asInterface(service);
+		}
+	};
+
+	private SunmyPrinter(){}
+	
+	
+	public static SunmyPrinter getInstance() {
+		return printer;
+	}
+	
+	public void initPrinter(Context context){
+		Intent intent=new Intent();
+		intent.setPackage("ru.fantom.sunmi.sunmyprinter");
+		intent.setAction("ru.fantom.sunmi.sunmyprinter.IWoyouService");
+		context.startService(intent);
+		context.bindService(intent, connService, Context.BIND_AUTO_CREATE);		
+	}
+	
+	public void print(String msg, ICallback callback){
+		if(woyouService != null){
+			try {
+				woyouService.printText(msg, callback);
+			} catch (RemoteException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+    
 }
