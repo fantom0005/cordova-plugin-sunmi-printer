e81f03e8d6f87db283a63b5885b2a29a0c7ca574
Anton Shvetsov <antonshvecow@gmail.com>
2016-11-22 15:33:00 +0700

    Synmi

--- src/android/cordova-plugin-sunmi-printer.java
+++ src/android/cordova-plugin-sunmi-printer.java
@@ -10,7 +10,7 @@ import org.json.JSONObject;
 /**
  * This class echoes a string called from JavaScript.
  */
-public class cordova-plugin-sunmi-printer extends CordovaPlugin {
+public class SunmyPrinter extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
